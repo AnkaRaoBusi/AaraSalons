fa489a946b0b6f6d027f6db805c92e11f035da4a
AnkaRaoBusi <ankimsdian7@gmail.com>
2025-12-12 14:48:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,11 @@ const cors = require('cors');
 const app = express();
 const allowedOrigins = [
   "https://aarasalons6.onrender.com",
-  "https://aarasalons5.onrender.com" ,// Render preview URL (update later)
-  // "https://www.aarasalons.com",         // your custom domain (later)
-  "http://localhost:4000"               // local dev (optional)
+  "https://aarasalons5.onrender.com",
+  "https://www.aarasalons.com",
+  "https://aarasalons.com",
+  "http://localhost:3000", // if your frontend runs locally on 3000
+  "http://localhost:4000"
 ];
 
 app.use(cors({
